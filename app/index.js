418b95ab4ddf58ae13f9c9d6ef2fc6a0c0853025
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-02-20 13:38:11 -0600

    added angular backbone lodash

--- app/index.js
+++ app/index.js
@@ -56,6 +56,18 @@ module.exports = yeoman.generators.Base.extend({
           name: 'Modernizr',
           value: 'includeModernizr',
           checked: true
+        }, {
+          name: 'Angular',
+          value: 'includeAngular',
+          checked: true
+        }, {
+          name: 'Backbone',
+          value: 'includeBackbone',
+          checked: true
+        },{
+          name: 'LoDash',
+          value: 'includeLodash',
+          checked: true
         }]
       },
       {
@@ -101,6 +113,9 @@ module.exports = yeoman.generators.Base.extend({
       // we change a bit this way of doing to automatically do this in the self.prompt() method.
       this.includeSass = hasFeature('includeSass');
       this.includeModernizr = hasFeature('includeModernizr');
+      this.includeAngular = hasFeature('includeAngular');
+      this.includeBackbone = hasFeature('includeBackbone');
+      this.includeLodash = hasFeature('includeLodash');
 
       this.includeReset     = (answers.css === 'reset');
       this.includeNormalize = (answers.css === 'normalize');
@@ -149,6 +164,18 @@ module.exports = yeoman.generators.Base.extend({
 
       if (this.includeModernizr) {
         bower.dependencies.modernizr = '~2.8.1';
+      }
+
+      if (this.includeAngular) {
+       bower.dependencies.angular =  '~1.4.0';
+      }
+
+      if (this.includeBackbone) {
+       bower.dependencies.backbone ='~1.1.2';
+      }
+
+      if (this.includeLodash) {
+       bower.dependencies.lodash ='~3.3.0';
       }
 
       if (this.includeNormalize) {
